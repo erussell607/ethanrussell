b021711b9b62468f97926965b58289202339c6a4
erussell607 <Ethan.Russell@mclaneco.com>
2018-11-24 01:45:15 -0500

    testing

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-const path = require('path');
 const express = require('express');
 const app = express();
-
-// Serve static files
-app.use(express.static(__dirname + '/dist/ethanrussell'));
-
-// Send all requests to index.html
+const path = require('path');
+app.use(express.static('./dist/ethanrussell'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/ethanrussell/index.html'));
+  res.sendFile(path.join('./dist/ethanrussell/index.html'));
 });
-
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
