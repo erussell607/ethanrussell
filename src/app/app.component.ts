851e59ddd2407068851afacf143e8042824f03fe
erussell607 <Ethan.Russell@mclaneco.com>
2018-11-24 16:31:23 -0500

    created the basic layout for angular-6-tutorial-new-features-power

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ethan Russell\'s Website';
+  title = 'Ethan Russell';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,58 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MyNavComponent } from './my-nav/my-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import {
+  MatToolbarModule,
+  MatButtonModule,
+  MatSidenavModule,
+  MatIconModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule
+} from '@angular/material';
+import { AddMemoryComponent } from './add-memory/add-memory.component';
+import { ViewMemoriesComponent } from './view-memories/view-memories.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyNavComponent,
+    AddMemoryComponent,
+    ViewMemoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: MyNavComponent, children: [
+          { path: 'add-memory', component: AddMemoryComponent },
+          { path: 'view-memories', component: ViewMemoriesComponent }
+        ]
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
