f62171726fb67f0b70f94a06009ffcb561cdd976
erussell607 <Ethan.Russell@mclaneco.com>
2018-11-24 01:26:05 -0500

    trying no dirname

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/ethanrussell'));
 
 app.get('/*all', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(path.join('/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
